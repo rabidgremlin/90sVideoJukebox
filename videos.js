0502f9b655bd112a3af840a5e99dba667d30e984
rabidgremlin <jack@rabidgremlin.com>
2019-08-24 16:43:38 +1200

    More songs.

--- videos.js
+++ videos.js
@@ -1,14 +1,180 @@
 var videos = [
     {
-        id: 'JgffRW1fKDk',
-        message: 'Republica - Ready to Go - requested by Bob'
+        id: '0hiUuL5uTKc',
+        message: 'Montell Jordan - This Is How We Do It - Kelly & Mark'
     },
     {
-        id: 'NdYWuo9OFAw',
-        message: 'Goo Goo Dolls - Iris - requested by Jane'
+        id: '-oqAU5VxFWs',
+        message: 'Counting Crows - Mr. Jones - Kelly & Mark'
     },
     {
-        id: 'DNPjeIamsck',
-        message: 'Ace of Base - The Sign - requested by Alice'
+        id: 'Z0hFQdEUQKM',
+        message: "Jane's Addiction - Jane Says - Kelly and Mark"
+    },
+    {
+        id: 'SGK00Q7xx-s',
+        message: "Korn - Blind - Chris"
+    },
+    {
+        id: 'W3q8Od5qJio',
+        message: "Rammstein - Du Hast - Chris"
+    },
+    {
+        id: 'kmZAsLMUuPw',
+        message: "Rage Against The Machine - Bullet In the Head - Chris"
+    },
+    {
+        id: 'vabnZ9-ex7o',
+        message: "Nirvana - Come As You Are - Ange"
+    },
+    {
+        id: '0Uc3ZrmhDN4',
+        message: "Sublime - What I Got - Ange"
+    },
+    {
+        id: 'NUTGr5t3MoY',
+        message: "Green Day - Basket Case - Ange"
+    },
+    {
+        id: 'fPO76Jlnz6c',
+        message: "Coolio - Gangsta's Paradise - Meegs"
+    },
+    {
+        id: 'MfmYCM4CS8o',
+        message: "Jon Bon Jovi - Blaze Of Glory - Meegs"
+    },
+    {
+        id: 'xSZBIs0gs0E',
+        message: "Natalie Imbruglia - Torn - Meegs"
+    },
+    {
+        id: 'X53ZSxkQ3Ho',
+        message: "Sir Mix-A-Lot - Baby Got Back - Emma"
+    },
+    {
+        id: 'gJLIiF15wjQ',
+        message: "Spice Girls - Wannabe - Emma"
+    },
+    {
+        id: 'rog8ou-ZepE',
+        message: "Vanilla Ice - Ice Ice Baby - Emma"
+    },
+    {
+        id: 'd-diB65scQU',
+        message: "Bobby McFerrin - Don't Worry Be Happy - Young"
+    },
+    {
+        id: 'JTMVOzPPtiw',
+        message: "Limp Bizkit - Nookie - Young"
+    },    
+    {
+        id: 'gRYZijLZR-Q',
+        message: "Third Eye Blind - Jumper - Gabrielle"
+    },
+    {
+        id: 'PHzOOQfhPFg',
+        message: "No Doubt - Just A Girl - Gabrielle"
+    },
+    {
+        id: 'r8OipmKFDeM',
+        message: "Oasis - Don't Look Back In Anger - Gabrielle"
+    },
+    {
+        id: 'Abrn8aVQ76Q',
+        message: "The Offspring - Self Esteem - Gabrielle"
+    },
+    {
+        id: 'Je-CPZRwbh8',
+        message: "Garbage - When I Grow Up - Troy & Lucy"
+    },
+    {
+        id: 'CnQ8N1KacJc',
+        message: "Green Day - Time Of Your Life - Troy & Lucy"
+    },
+    {
+        id: '9Ht5RZpzPqw',
+        message: "blink-182 - All The Small Things - Troy & Lucy"
+    },
+    {
+        id: '0-EF60neguk',
+        message: "Sinead O'Connor - Nothing Compares 2U - Sally & Bruce"
+    },
+    {
+        id: 'xwtdhWltSIg',
+        message: "R.E.M. - Losing My Religion - Sally & Bruce"
+    },
+    {
+        id: 'FsRHQt1_nfk',
+        message: "Chumbawamba - Tubthumping - Sally & Bruce"
+    },
+    {
+        id: 'ruAi4VBoBSM',
+        message: "Fatboy Slim - Praise You - Aileen & Dan"
+    },
+    {
+        id: 'otCpCn0l4Wo',
+        message: "MC Hammer - U Can't Touch - Aileen & Dan"
+    },
+    {
+        id: 'XFkzRNyygfk',
+        message: "Radiohead - Creep - Aileen & Dan"
+    },
+    {
+        id: '8WEtxJ4-sh4',
+        message: "TLC - Waterfalls - Nick & Amber"
+    },
+    {
+        id: 'hTWKbfoikeg',
+        message: "Nirvana - Smells Like Teen Spirit - Nick & Amber"
+    },
+    {
+        id: '9cQlVww0zKo',
+        message: "Sash! - Ecuador - Nick & Amber"
+    },
+    {
+        id: 'K7l5ZeVVoCA',
+        message: "blink-182 - What's My Age Again? - Simon"
+    },
+    {
+        id: 'ENXvZ9YRjbo',
+        message: "Weezer - Say It Ain't So - Simon"
+    },
+    {
+        id: 'sT0g16_LQaQ',
+        message: "blink-182 - Dammit - Simon"
+    },
+    {
+        id: 'ie_rslhsYns',
+        message: "Metallica - Astronomy - Sam"
+    },
+    {
+        id: 'wgLFq2SVAOE',
+        message: "Supergroove - Scorpio Girls - Sam"
+    },
+    {
+        id: 'sjHYAVeFwGw',
+        message: "Pennywise - Bro Hymn - Sam"
+    },
+    {
+        id: 'RijB8wnJCN0',
+        message: "Cypress Hill - Insane In The Brain - James & Carolynne"
+    },
+    {
+        id: 'MK1g5dMYR3s',
+        message: "Duran Duran - Electric Barbarella - James & Carolynne"
+    },
+    {
+        id: 'eBG7P-K-r1Y',
+        message: "Foo Fighters - Everlong - James & Carolynne"
+    },
+    {
+        id: 'i7mEB2wnDLQ',
+        message: "Suede - Animal Nitrate - James & Carolynne"
     }
+
+
+
+
+
+
 ]
